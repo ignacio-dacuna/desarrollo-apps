86a33ec4412656cf92f6fc8d5e36a4d4ccd1df06
ignacio-dacuna <ignaciodacuna9@gmail.com>
2024-01-19 12:22:32 -0300

    terminando la primera entrega

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola, Coder</Text>
+      <Text></Text>
+      <Text></Text>
+      <Text>esta es mi primera entrega del curso de:</Text>
+      <Text>desarrolo de apps</Text>
       <StatusBar style="auto" />
     </View>
   );
